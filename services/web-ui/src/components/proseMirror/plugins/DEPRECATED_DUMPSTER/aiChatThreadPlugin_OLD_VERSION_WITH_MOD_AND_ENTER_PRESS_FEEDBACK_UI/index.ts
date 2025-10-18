1d826da6c9305090af5052a98ae770d0737d4421
Shelby Carter <lixpi@lixpi.com>
2025-10-18 00:13:27 -0400

    cleanup

--- services/web-ui/src/components/proseMirror/plugins/DEPRECATED_DUMPSTER/aiChatThreadPlugin_OLD_VERSION_WITH_MOD_AND_ENTER_PRESS_FEEDBACK_UI/index.ts
+++
@@ -1,9 +0,0 @@
-// AI Chat Thread Plugin - Complete Export
-// This file exports all functionality from the AI chat thread plugin
-
-// Export all from node definitions
-export * from './aiChatThreadNode.ts'
-export * from './aiResponseMessageNode.ts'
-
-// Export all from plugin
-export * from './aiChatThreadPlugin.ts'
